f666bc0762e1737696286ff0c136698f0982c4e8
Jake Brown <jacob.f.brown1991@gmail.com>
2025-03-18 16:31:00 -0700

    i cant even

---
+++ src/app/extensions/config.js
@@ -0,0 +1,3 @@
+// Hard-code your Google Maps API key here for development
+// In production, this should be replaced with the value from HubSpot secrets
+export const GOOGLE_MAPS_API_KEY = "AIzaSyBOR4aRTh5R5B3M7r1BhSZBGIbKZHN_mwA";
